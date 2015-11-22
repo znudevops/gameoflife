0f6d9bc2af3658084b2b93e26e48dada5c41c095
Frans Guelinckx <frans.guelinckx@gmail.com>
2015-11-22 21:51:56 +0100

    add angular app to spring boot

---
+++ src/main/resources/static/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gameoflifeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gameoflifeApp
+ */
+angular.module('gameoflifeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
