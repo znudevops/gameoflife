8c12ec048f3c43cc92d3b73370564d25f1397eeb
Frans Guelinckx <frans.guelinckx@gmail.com>
2016-01-05 19:58:20 +0100

    send current world to backend + revamp frontend

--- src/main/java/be/cegeka/status/StatusController.java
+++ src/main/java/be/cegeka/status/StatusController.java
@@ -1,11 +1,15 @@
 package be.cegeka.status;
 
-import jersey.repackaged.com.google.common.collect.Lists;
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static jersey.repackaged.com.google.common.collect.Lists.newArrayList;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by fransg on 02/12/15.
@@ -14,18 +18,24 @@ import java.util.List;
 @RequestMapping(value = "/status")
 public class StatusController {
 
+    private static Logger logger = Logger.getLogger(StatusController.class);
+
     private List<List<Boolean>> world;
+    private List<List<Boolean>> oldWorld;
 
     @RequestMapping(value = "/name")
     @ResponseBody
     public String getApplicationName(){
         return "{\"name\": \"Game of Life\"}";
     }
-    @RequestMapping(value = "/world")
+
+    @RequestMapping(value = "/world", method = POST)
     @ResponseBody
-    public List<List<Boolean>> getWorld(){
-        List<Boolean> row = Lists.newArrayList(false, true, true, false, false);
-        world = Lists.newArrayList(row, row, row);
+    public List<List<Boolean>> getWorld(@RequestBody List<List<Boolean>> oldWorld){
+        logger.info(oldWorld);
+        this.oldWorld = oldWorld;
+        List<Boolean> row = newArrayList(false, true, true, false, false);
+        world = newArrayList(row, row, row, row, row, row, row);
         return world;
     }
 }
--- src/main/webapp/scripts/controllers/main.js
+++ src/main/webapp/scripts/controllers/main.js
@@ -3,12 +3,16 @@
 angular.module('gameoflifeApp')
     .controller('MainCtrl', function ($scope, $http, $log) {
 
-        $http.get("/status/name").success(function(data){
+        $scope.world = [[true, false, false, true, false], [true, false, false, true, false], [true, false, false, true, false], [true, false, false, true, false], [true, false, false, true, false]];
+
+        $http.get("/status/name").success(function (data) {
             $scope.title = data.name;
         });
 
-        $http.get("/status/world").success(function(data){
-            $scope.world = data;
-            $log.log($scope.world);
-        });
+        $http
+            .post("/status/world", $scope.world)
+            .success(function (data) {
+                $scope.world = data;
+                $log.info(data);
+            });
     });
