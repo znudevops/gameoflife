e962ff6f987b9d79ced32d62bbbe17a15cda8ae7
robs <rob.swartenbroekx@cegeka.com>
2017-03-25 09:32:47 +0100

    Refactoring neighbourhoodfactory

--- src/main/java/be/cegeka/gameoflife/NeighbourHoodFactory.java
+++ src/main/java/be/cegeka/gameoflife/NeighbourHoodFactory.java
@@ -7,103 +7,109 @@ import java.util.stream.Collectors;
 
 public class NeighbourHoodFactory {
 
-    public static Neighbourhood create(List<List<Boolean>> currentWorld, Location location) {
-        return new Neighbourhood((int) getNeighbours(currentWorld, location).stream().filter(neighbor -> neighbor == Cel.LIVING).count());
+    private final List<List<Boolean>> currentWorld;
+
+    public NeighbourHoodFactory(List<List<Boolean>> currentWorld) {
+        this.currentWorld = currentWorld;
     }
 
-    private static List<Cel> getNeighbours(List<List<Boolean>> currentWorld, Location location) {
-        return Arrays.asList(getDownStairNeighbours(currentWorld, location), getFellowNeighbors(currentWorld, location), getUpperNeighbors(currentWorld, location)).stream().flatMap(Collection::stream).collect(Collectors.toList());
+    public Neighbourhood create(Location location) {
+        return new Neighbourhood((int) getNeighbours(location).stream().filter(neighbor -> neighbor == Cel.LIVING).count());
     }
 
-    private static List<Cel> getDownStairNeighbours(List<List<Boolean>> currentWorld, Location location) {
-        return Arrays.asList(getFirstDownstairNeighbor(currentWorld, location), getSecondDownstairNeighbor(currentWorld, location), getThirdDownstairNeighbor(currentWorld, location));
+    private List<Cel> getNeighbours(Location location) {
+        return Arrays.asList(getDownStairNeighbours(location), getFellowNeighbors(location), getUpperNeighbors(location)).stream().flatMap(Collection::stream).collect(Collectors.toList());
     }
 
-    private static List<Cel> getFellowNeighbors(List<List<Boolean>> currentWorld, Location location) {
-        return Arrays.asList(getFirstFellowNeighbor(currentWorld, location), getSecondFellowNeigbor(currentWorld, location));
+    private List<Cel> getDownStairNeighbours(Location location) {
+        return Arrays.asList(getFirstDownstairNeighbor(location), getSecondDownstairNeighbor(location), getThirdDownstairNeighbor(location));
     }
 
-    private static List<Cel> getUpperNeighbors(List<List<Boolean>> currentWorld, Location location) {
-        return Arrays.asList(getFirstUpperNeighbor(currentWorld, location), getSecondUpperNeighbor(currentWorld, location), getThirdUpperNeighbor(currentWorld, location));
+    private List<Cel> getFellowNeighbors(Location location) {
+        return Arrays.asList(getFirstFellowNeighbor(location), getSecondFellowNeigbor(location));
     }
 
-    private static Cel getFirstUpperNeighbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationFirstUpperNeighbor(location));
+    private List<Cel> getUpperNeighbors(Location location) {
+        return Arrays.asList(getFirstUpperNeighbor(location), getSecondUpperNeighbor(location), getThirdUpperNeighbor(location));
     }
 
-    private static Cel getSecondUpperNeighbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationSecondUpperNeighbor(location));
+    private Cel getFirstUpperNeighbor(Location location) {
+        return getCel(getLocationFirstUpperNeighbor(location));
     }
 
-    private static Cel getThirdUpperNeighbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationThirdUpperNeighbor(location));
+    private Cel getSecondUpperNeighbor(Location location) {
+        return getCel(getLocationSecondUpperNeighbor(location));
     }
 
-    private static Cel getFirstFellowNeighbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationFirstFellowNeighbor(location));
+    private Cel getThirdUpperNeighbor(Location location) {
+        return getCel(getLocationThirdUpperNeighbor(location));
     }
 
-    private static Cel getSecondFellowNeigbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationSecondFellowNeighbor(location));
+    private Cel getFirstFellowNeighbor(Location location) {
+        return getCel(getLocationFirstFellowNeighbor(location));
     }
 
-    private static Cel getFirstDownstairNeighbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationFirstDownstairNeighbor(location));
+    private Cel getSecondFellowNeigbor(Location location) {
+        return getCel(getLocationSecondFellowNeighbor(location));
     }
 
-    private static Cel getSecondDownstairNeighbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationSecondDownstairNeighbor(location));
+    private Cel getFirstDownstairNeighbor(Location location) {
+        return getCel(getLocationFirstDownstairNeighbor(location));
     }
 
-    private static Cel getThirdDownstairNeighbor(List<List<Boolean>> currentWorld, Location location) {
-        return getCel(currentWorld, getLocationThirdDownstairNeighbor(location));
+    private Cel getSecondDownstairNeighbor(Location location) {
+        return getCel(getLocationSecondDownstairNeighbor(location));
     }
 
-    private static Cel getCel(List<List<Boolean>> currentWorld, Location location) {
-        return doesCelExist(currentWorld, location) ? Cel.of(currentWorld.get(location.getRowNummer()).get(location.getColumnNummer())) : Cel.DEAD;
+    private Cel getThirdDownstairNeighbor(Location location) {
+        return getCel(getLocationThirdDownstairNeighbor(location));
     }
 
-    private static boolean doesCelExist(List<List<Boolean>> currentWorld, Location location) {
-        return doesRowExist(currentWorld, location) && doesColumnExist(currentWorld, location);
+    private Cel getCel(Location location) {
+        return doesCelExist(location) ? Cel.of(currentWorld.get(location.getRowNummer()).get(location.getColumnNummer())) : Cel.DEAD;
     }
 
-    private static boolean doesColumnExist(List<List<Boolean>> currentWorld, Location location) {
+    private boolean doesCelExist(Location location) {
+        return doesRowExist(location) && doesColumnExist(location);
+    }
+
+    private boolean doesColumnExist(Location location) {
         return location.getColumnNummer() >= 0 && location.getColumnNummer() < currentWorld.size();
     }
 
-    private static boolean doesRowExist(List<List<Boolean>> currentWorld, Location location) {
+    private boolean doesRowExist(Location location) {
         return location.getRowNummer() >= 0 && location.getRowNummer() < currentWorld.get(0).size();
     }
 
-    private static Location getLocationFirstUpperNeighbor(Location location) {
+    private Location getLocationFirstUpperNeighbor(Location location) {
         return new Location(location.getRowNummer() - 1, location.getColumnNummer() - 1);
     }
 
-    private static Location getLocationSecondUpperNeighbor(Location location) {
+    private Location getLocationSecondUpperNeighbor(Location location) {
         return new Location(location.getRowNummer() - 1, location.getColumnNummer());
     }
 
-    private static Location getLocationThirdUpperNeighbor(Location location) {
+    private Location getLocationThirdUpperNeighbor(Location location) {
         return new Location(location.getRowNummer() - 1, location.getColumnNummer() + 1);
     }
 
-    private static Location getLocationFirstFellowNeighbor(Location location) {
+    private Location getLocationFirstFellowNeighbor(Location location) {
         return new Location(location.getRowNummer(), location.getColumnNummer() - 1);
     }
 
-    private static Location getLocationSecondFellowNeighbor(Location location) {
+    private Location getLocationSecondFellowNeighbor(Location location) {
         return new Location(location.getRowNummer(), location.getColumnNummer() + 1);
     }
 
-    private static Location getLocationFirstDownstairNeighbor(Location location) {
+    private Location getLocationFirstDownstairNeighbor(Location location) {
         return new Location(location.getRowNummer() + 1, location.getColumnNummer() - 1);
     }
 
-    private static Location getLocationSecondDownstairNeighbor(Location location) {
+    private Location getLocationSecondDownstairNeighbor(Location location) {
         return new Location(location.getRowNummer() + 1, location.getColumnNummer());
     }
 
-    private static Location getLocationThirdDownstairNeighbor(Location location) {
+    private Location getLocationThirdDownstairNeighbor(Location location) {
         return new Location(location.getRowNummer() + 1, location.getColumnNummer() + 1);
     }
 }
--- src/test/java/be/cegeka/gameoflife/NeighbourhoodFactoryTest.java
+++ src/test/java/be/cegeka/gameoflife/NeighbourhoodFactoryTest.java
@@ -9,58 +9,58 @@ import java.util.List;
 
 public class NeighbourhoodFactoryTest {
 
-    private List<List<Boolean>> currentWorld;
+    private NeighbourHoodFactory neighbourHoodFactory;
 
     @Before
     public void setup() {
-        currentWorld = Arrays.asList(
+        neighbourHoodFactory = new NeighbourHoodFactory(Arrays.asList(
             Arrays.asList(true, false, true),
             Arrays.asList(false, true, false),
-            Arrays.asList(true, false, true));
+            Arrays.asList(true, false, true)));
     }
 
     @Test
     public void givenLocation00_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(0, 0)).getAmountOfLivingNeigbours()).isEqualTo(1);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(0, 0)).getAmountOfLivingNeigbours()).isEqualTo(1);
     }
 
     @Test
     public void givenLocation01_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(0, 1)).getAmountOfLivingNeigbours()).isEqualTo(3);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(0, 1)).getAmountOfLivingNeigbours()).isEqualTo(3);
     }
 
     @Test
     public void givenLocation02_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(0, 2)).getAmountOfLivingNeigbours()).isEqualTo(1);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(0, 2)).getAmountOfLivingNeigbours()).isEqualTo(1);
     }
 
     @Test
     public void givenLocation10_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(1, 0)).getAmountOfLivingNeigbours()).isEqualTo(3);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(1, 0)).getAmountOfLivingNeigbours()).isEqualTo(3);
     }
 
     @Test
     public void givenLocation11_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(1, 1)).getAmountOfLivingNeigbours()).isEqualTo(4);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(1, 1)).getAmountOfLivingNeigbours()).isEqualTo(4);
     }
 
     @Test
     public void givenLocation12_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(1, 2)).getAmountOfLivingNeigbours()).isEqualTo(3);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(1, 2)).getAmountOfLivingNeigbours()).isEqualTo(3);
     }
 
     @Test
     public void givenLocation20_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(2, 0)).getAmountOfLivingNeigbours()).isEqualTo(1);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(2, 0)).getAmountOfLivingNeigbours()).isEqualTo(1);
     }
 
     @Test
     public void givenLocation21_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(2, 1)).getAmountOfLivingNeigbours()).isEqualTo(3);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(2, 1)).getAmountOfLivingNeigbours()).isEqualTo(3);
     }
 
     @Test
     public void givenLocation22_whenCreatingNeighbourhood_NeigbourhoodWithCorrectAmountOfLivingNeigborsIsCreated() {
-        Assertions.assertThat(NeighbourHoodFactory.create(currentWorld, new Location(2, 2)).getAmountOfLivingNeigbours()).isEqualTo(1);
+        Assertions.assertThat(neighbourHoodFactory.create(new Location(2, 2)).getAmountOfLivingNeigbours()).isEqualTo(1);
     }
 }
